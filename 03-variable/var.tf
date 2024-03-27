493a87cc847399930e81fe1b35391936d6533240
Anand <anandarao.pappala@onpassive.com>
2024-03-27 16:52:56 +0530

    variableupdate

--- 03-variable/var.tf
+++ 03-variable/var.tf
@@ -3,7 +3,26 @@ variable "x" {
   
 }
 
-output "variable" {
+variable "y_list" {
+    default = [1,2,3]
+      
+}
+variable "z_mab" {
+    default = {
+        x = 10
+        y = 11
+    }
+}
+output "x" {
     value = var.x
  
+}
+
+output "y" {
+    value = var.y_list
+  
+}
+output "z" {
+    value = var.z_mab
+  
 }
