2a4404e36239af134497093c6990df814a3f30ed
Anand <anandarao.pappala@onpassive.com>
2024-03-28 16:00:24 +0530

    declare variable

--- 05-module/demo/test/variable.tf
+++ 05-module/demo/test/variable.tf
@@ -1,13 +1,13 @@
 variable "instance_type" {
- 
+ default = "t2.medium"
 
 }
 variable "ami" {
-  
+  default = "ami-01770af15e887950c"
    
   
 }
 
 variable "tags" { 
- 
+ default = "samepage"
 }
