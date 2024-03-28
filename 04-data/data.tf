634a6893875ef0dc6df5907af4ba079a4b6d2880
Anand <anandarao.pappala@onpassive.com>
2024-03-28 12:19:54 +0530

    test5

--- 04-data/data.tf
+++ 04-data/data.tf
@@ -2,7 +2,9 @@ data "aws_security_group" "selected" {
   id = var.security_group_id
 }
 
-variable "security_group_id" {}
+variable "security_group_id" {
+    default = "sg-0049c64187a56a781"
+}
 
 output "sg" {
     value = data.aws_security_group.selected
