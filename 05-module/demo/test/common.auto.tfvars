87d7c4ce54baee257647c9ea5d2595e4182a4127
Anand <anandarao.pappala@onpassive.com>
2024-03-28 15:50:08 +0530

    declare variable

--- 05-module/demo/test/common.auto.tfvars
+++
@@ -1,3 +0,0 @@
-instance_type = "t2.medium"
-ami = "ami-01770af15e887950c"
-tags = "tagvar"
--- 06-module1/main.tf
+++ 06-module1/main.tf
@@ -1,5 +1,11 @@
-module "ec2instance" {
-source = "../05-module/demo/test"
+module "demo" {
+  source = "../05-module/demo/test"
 
+instance_type = "t2.medium"
+ami = "ami-01770af15e887950c"
+tags = "testing"
+  
 }
+
+
 
