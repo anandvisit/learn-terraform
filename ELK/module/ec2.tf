af889412b8f3054d91c91f46b648ec4583594a89
Anand <anandarao.pappala@onpassive.com>
2024-04-02 15:41:37 +0530

    update

--- ELK/module/ec2.tf
+++ ELK/module/ec2.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "name" {
     ami = var.imageid
     instance_type = var.instance_type
-    security_groups = var.security_group_name
+  ##  security_groups = var.security_group_name
+    vpc_security_group_ids = var.security_group_name
    
     tags = {
       Name = var.tags
