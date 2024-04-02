dc0eb1431a669d1a92d4338f3cbe307d17ca35d5
Anand <anandarao.pappala@onpassive.com>
2024-04-02 15:36:45 +0530

    ec2 update

--- ELK/main.tf
+++ ELK/main.tf
@@ -3,7 +3,7 @@ module "ec2" {
 
 instance_type = "t2.medium"
 ami = "ami-01770af15e887950c"
-security_group_id = "sg-0dbd6bb42cce6fcc2"
+security_group_name = "sg-0dbd6bb42cce6fcc2"
 vpc_id = "vpc-0a0ebee1193401bf0"
 tags = "module-frontend"
 imageid = ""
