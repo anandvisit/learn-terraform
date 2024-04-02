2a13e74dd66b39b55c9936b7e6b0b260bdf4e32d
Anand <anandarao.pappala@onpassive.com>
2024-04-02 18:02:31 +0530

    update

--- securitygroup/main.tf
+++ securitygroup/main.tf
@@ -3,6 +3,10 @@ data "aws_security_group" "sg" {
 }
 
 variable "aws_security_group" {
-    default = ""
+    default = "sg-0dbd6bb42cce6fcc2"
     
+}
+output "sg_name" {
+    value = var.aws_security_group
+  
 }
