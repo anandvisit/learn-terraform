fc0b77947fff5bff56227826b5d6c5dcb224a5f7
Anand <anandarao.pappala@onpassive.com>
2024-04-04 14:58:29 +0530

    updated website

--- s3/main.tf
+++ s3/main.tf
@@ -28,3 +28,15 @@ resource "aws_s3_bucket_public_access_block" "example" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
