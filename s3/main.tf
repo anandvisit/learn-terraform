9a476faa2b5c15d4654b63bf1260d14663dcd666
Anand <anandarao.pappala@onpassive.com>
2024-04-04 14:24:41 +0530

    updated

--- s3/main.tf
+++ s3/main.tf
@@ -1,8 +1,22 @@
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket-bkt"
+  
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
+}
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
 }
