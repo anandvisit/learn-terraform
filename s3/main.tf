9d650afccf3c49929454a8694f4155d2d70136dd
Anand <anandarao.pappala@onpassive.com>
2024-04-04 14:39:16 +0530

    updated

--- s3/main.tf
+++ s3/main.tf
@@ -20,3 +20,11 @@ resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.example.id
   acl    = "private"
 }
+resource "aws_s3_bucket_public_access_block" "example" {
+     bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
